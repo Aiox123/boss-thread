0c0658ff9541435ba5a7d92aa54044867afe07b8
nean <2801912027@qq.com>
2023-02-09 22:28:01 +0800

    add Redis 提高系统整体性能

--- src/main/java/cn/nean/boss/service/impl/ThreadServiceImpl.java
+++ src/main/java/cn/nean/boss/service/impl/ThreadServiceImpl.java
@@ -1,24 +1,26 @@
 package cn.nean.boss.service.impl;
 
+import cn.nean.boss.mapper.BlogMapper;
 import cn.nean.boss.service.ThreadService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.TimeUnit;
 
 @Component
 @Slf4j
 public class ThreadServiceImpl implements ThreadService {
 
+    @Autowired
+    BlogMapper blogMapper;
+
     @Override
     @Async("taskExecutor")
     public void updateBlogComments(long blogId) {
-        try {
-            TimeUnit.SECONDS.sleep(3);
+        int i = blogMapper.updateCommentsById(blogId);
+        if(i > 0){
             log.info("文章ID: {} 点赞数量更新完毕!",blogId);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
         }
     }
 }
--- src/main/java/cn/nean/boss/web/BlogApi.java
+++ src/main/java/cn/nean/boss/web/BlogApi.java
@@ -1,10 +1,13 @@
 package cn.nean.boss.web;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
 import cn.nean.boss.mapper.BlogMapper;
 import cn.nean.boss.model.pojo.Blog;
 import cn.nean.boss.service.ThreadService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/blog")
 public class BlogApi {
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
 
     @Autowired
     ThreadService threadService;
@@ -24,7 +30,17 @@ public class BlogApi {
     public Blog queryDetailsBlog(){
         long blogId = 1;
         threadService.updateBlogComments(blogId);
+        String key = "blog:" + blogId;
+        // 线程 Redis 缓存
+        String blogJson = stringRedisTemplate.opsForValue().get(key);
+        if(StrUtil.isNotBlank(blogJson)){
+            Blog blog = JSONUtil.toBean(blogJson, Blog.class);
+            log.info("缓存博客详情存在: {}",blog);
+            return blog;
+        }
+
         Blog blog = blogMapper.queryBlogById(blogId);
+        stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(blog));
         log.info("博客详情: {}",blog);
         return blog;
     }
